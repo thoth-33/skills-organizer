65f3f147f6036fea43dd241afc43477f4528c17a
thoth-33 <146050985+thoth-33@users.noreply.github.com>
2025-05-19 18:46:28 -0500

    Revert "Update SkillsOrganizerPlugin.java"
    This reverts commit fa9acdf27a6f4bf16ffdbda700a27f6bf28e5aa6.

--- src/main/java/io/robrichardson/skillsorganizer/SkillsOrganizerPlugin.java
+++ src/main/java/io/robrichardson/skillsorganizer/SkillsOrganizerPlugin.java
@@ -108,13 +108,7 @@ public class SkillsOrganizerPlugin extends Plugin
 			skillTile.setXPositionMode(WidgetPositionMode.ABSOLUTE_LEFT);
 			skillTile.setYPositionMode(WidgetPositionMode.ABSOLUTE_TOP);
 			skillTile.setOriginalX(skillPositionConfig.getX());
-
-			// 🎯 Special-case the 2nd skill (idx = 1) to move it 1px lower
-			int y = skillPositionConfig.getY();
-			if (idx == 1) {
-				y += 1;
-			}
-			skillTile.setOriginalY(y);
+			skillTile.setOriginalY(skillPositionConfig.getY());
 			skillTile.revalidate();
 
 			SkillWidgetGroup widgetGroup = groups[idx];
